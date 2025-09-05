144bb39428d7bd7ceae23138d09aa9ce72ee26f2
Gerard Ballestero Vidal <6224745@cegepmontpetit.ca>
2025-09-05 09:19:40 -0400

    FCT
    Requête pour recuperer les chansons d'un album

--- TP1/src/app/app.ts
+++ TP1/src/app/app.ts
@@ -19,17 +19,31 @@ export class App {
 
   constructor(public http : HttpClient){}
 
+  serchName : string = "";
   artistName : string = "";
   albumList : Album[] = [];
   songList : Song[] = [];
+  message : string = "";
 
   async getArtistAlbum(){
+    this.artistName = this.serchName;
     this.albumList = [];
     let x = await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="+ this.artistName +"&api_key=9a8a3facebbccaf363bb9fd68fa37abf&format=json"));
+    this.serchName = "";
     console.log(x);
     for(let alb of x.topalbums.album)
     {
       this.albumList.push(new Album(alb.name, alb.image[2]['#text']))
+    }
+  }
+
+  async getAlbumSongs(albumName : string){
+    this.songList = [];
+    let x = await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=9a8a3facebbccaf363bb9fd68fa37abf&artist="+ this.artistName +"&album="+ albumName  +"&format=json"));
+    console.log(x);
+    for(let song of x.album.tags.tag)
+    {
+      this.songList.push(new Song(song.name))
     }
   }
 }
